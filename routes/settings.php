26f274067bf37c3d168a9403a5caf8f41373c507
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2026-03-22 19:29:52 +0700

    build: initialize project

---
+++ routes/settings.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Settings\ProfileController;
+use App\Http\Controllers\Settings\SecurityController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('settings')->middleware(['auth'])->group(function () {
+    Route::redirect('', '/settings/profile');
+
+    Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::middleware('verified')->group(function () {
+        Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        Route::get('security', [SecurityController::class, 'edit'])->name('security.edit');
+
+        Route::put('password', [SecurityController::class, 'update'])
+            ->middleware('throttle:6,1')
+            ->name('user-password.update');
+
+        Route::inertia('appearance', 'settings/appearance')->name('appearance.edit');
+    });
+});
